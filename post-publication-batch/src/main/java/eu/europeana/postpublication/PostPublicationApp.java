8ecd246939d2be5ea8e4a127bd39d505e5778951
srishti <srishti.singh@europeana.eu>
2023-06-12 12:33:25 +0200

    EA-3451 - pushing logs testing

--- post-publication-batch/src/main/java/eu/europeana/postpublication/PostPublicationApp.java
+++ post-publication-batch/src/main/java/eu/europeana/postpublication/PostPublicationApp.java
@@ -2,6 +2,9 @@ package eu.europeana.postpublication;
 
 import eu.europeana.postpublication.config.SocksProxyActivator;
 import eu.europeana.postpublication.config.SocksProxyConfig;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
@@ -15,6 +18,10 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Main application
@@ -41,6 +48,8 @@ import org.springframework.context.ConfigurableApplicationContext;
 @EnableBatchProcessing
 public class PostPublicationApp {
 
+    private static final Logger logger = LogManager.getLogger(PostPublicationApp.class);
+
     /**
      * This method is called when starting as a Spring-Boot application (e.g. from your IDE)
      *
@@ -52,12 +61,58 @@ public class PostPublicationApp {
         SocksProxyActivator.activate(
                 new SocksProxyConfig("post-publication.properties", "post-publication.user.properties"));
 
-        // disable web server since we're only running an update task
-        ConfigurableApplicationContext context =
-                new SpringApplicationBuilder(PostPublicationApp.class)
-                        .web(WebApplicationType.NONE)
-                        .run(args);
-        System.exit(SpringApplication.exit(context));
+        if (hasCmdLineParams(args)) {
+            logger.info("Running the application with arguments -  args: {}", args);
+            validateArguments(args);
+            InputStream streamReader = PostPublicationApp.class.getResourceAsStream("/logs/TestLog.log");
+            readLogFile(streamReader);
+        }
+        else {
+            // disable web server since we're only running an update task
+            ConfigurableApplicationContext context =
+                    new SpringApplicationBuilder(PostPublicationApp.class)
+                            .web(WebApplicationType.NONE)
+                            .run(args);
+            System.exit(SpringApplication.exit(context));
+        }
+    }
+
+
+    static boolean hasCmdLineParams(String[] args) {
+        return args!=null && args.length > 0;
+    }
+
+    /** validates the arguments passed */
+    private static void validateArguments(String[] args) {
+        for (String arg : args) {
+            if (!StringUtils.equals(arg, "pushLogs")) {
+                logger.error("Unsupported argument '{}'. Supported arguments are '{}'",
+                        arg, "pushLogs");
+                System.exit(1);
+            }
+        }
+    }
+
+
+    /**
+     * Reads the FailedSetsReport generated at that time
+     *
+     * @return list of all the failed sets
+     * returns "", if file is empty.
+     */
+    public static List<String> readLogFile(InputStream stream) {
+        List<String> failedSets = new ArrayList<>();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"))) {
+            String  nextLine;
+            while ((nextLine = reader.readLine()) != null) {
+                System.out.println(nextLine);
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("file doesn't exist." );
+        } catch (IOException e) {
+            logger.error("Error reading the file", e);
+        }
+        return failedSets;
     }
 
 
